2b0184c60b721f148ccab4fa4670bd48f093b017
janisgee <janisgeegee@gmail.com>
2023-07-15 23:25:02 +0800

    restructure the code with different components and files

--- src/Main.js
+++ src/Main.js
@@ -1,72 +1,26 @@
 import { useState } from 'react';
 import Box from './Box';
 import MovieDetails from './MoviesDetails/MovieDetails';
-// import MovieDetails from './MoviesDetails/MoviesDetails';
-
-const average = (arr) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+import WatchedSummary from './Watched/WatchedSummary';
+import WatchedMoviesList from './Watched/WatchedMoviesList';
 
 export default function Main({ movies, tempWatchedData }) {
   const [watched, setWatched] = useState(tempWatchedData);
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
   return (
     <main className='main'>
       <Box>
         <ul className='list list-movies'>
           {movies?.map((movie) => (
-            <MovieDetails movie={movie} />
+            <MovieDetails movie={movie} key={movie.imdbID} />
           ))}
         </ul>
       </Box>
 
       <Box>
         <>
-          <div className='summary'>
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>#️⃣</span>
-                <span>{watched.length} movies</span>
-              </p>
-              <p>
-                <span>⭐️</span>
-                <span>{avgImdbRating}</span>
-              </p>
-              <p>
-                <span>🌟</span>
-                <span>{avgUserRating}</span>
-              </p>
-              <p>
-                <span>⏳</span>
-                <span>{avgRuntime} min</span>
-              </p>
-            </div>
-          </div>
-          <ul className='list list-movies'>
-            {watched.map((movie) => (
-              <li key={movie.imdbID}>
-                <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                <h3>{movie.Title}</h3>
-                <div>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{movie.imdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{movie.userRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{movie.runtime} min</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
         </>
       </Box>
     </main>
--- src/MoviesDetails/MovieDetails.js
+++ src/MoviesDetails/MovieDetails.js
@@ -1,6 +1,6 @@
 export default function MovieDetails({ movie }) {
   return (
-    <li key={movie.imdbID}>
+    <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
